781f0d1dbff97c10dc76de8c8d700d50cc8af57c
wellbell <uok02018496@naver.com>
2021-05-22 23:22:51 +0900

    [refactor] RedisConnectionFactory 빈 설정 매개 변수 수정
    LettuceConnectionFactory 생성자에 host, port를 넣을 경우 RedisStandaloneConfiguration로 자동 생성되기 때문에 굳이 new 로 생성 할필요 없음

--- src/main/java/com/redis/study/config/RedisConfig.java
+++ src/main/java/com/redis/study/config/RedisConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -21,7 +20,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
